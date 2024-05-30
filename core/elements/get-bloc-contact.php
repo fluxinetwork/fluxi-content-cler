aa151c9a0548ac1f3a9a1916c5e549e22ca68274
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2024-05-30 14:50:24 +0200

    Bloc contact
    email txt normal

--- core/elements/get-bloc-contact.php
+++ core/elements/get-bloc-contact.php
@@ -12,13 +12,13 @@
 	 */
 
 	function get_bloc_contact(){
-			
+
 		$equipier_referent = get_sub_field('equipier_referent');
 		$equipier_id = $equipier_referent[0];
-		$descriptif_contact = get_sub_field('descriptif_contact');	
-	
+		$descriptif_contact = get_sub_field('descriptif_contact');
 
-		if( $equipier_referent ):			
+
+		if( $equipier_referent ):
 			$photo = get_field('photo', $equipier_id);
 			global $isMobile;
 			($isMobile) ? $img_size = 'thumb2x' : $img_size = 'thumbnail';
@@ -29,14 +29,14 @@
 			$output .= '<span class="c-newsH__body__meta">Contact</span>';
 			$output .= '<h1 class="c-newsH__body__title">'.get_the_title($equipier_id).'</h1>';
 			$output .= '<p class="c-newsH__body__desc">'.$descriptif_contact.'</p>';
-			$output .= '<span class="c-link c-link--more c-newsH__body__link">'.get_field('mail_contact', $equipier_id).'</span>';
+			$output .= '<span class="c-link c-link--more -email c-newsH__body__link">'.get_field('mail_contact', $equipier_id).'</span>';
 			$output .= '</div>';
 			$output .= '</div>';
 
 			return $output;
-			
+
 		endif;
-		
+
 	}
 
 ?>
